4ddb2f380c8849fbe15ff58eec8978ac30685c72
claimer <claimer@claimer.claimer>
2025-04-10 16:07:15 +0300

    a

--- claim_eg.sh
+++ claim_eg.sh
@@ -3,6 +3,4 @@
 # Navigate to the directory containing free-games-claimer scripts
 cd ~/free-games-claimer
 
-# Run the Node.js scripts sequentially
-node gog
 node epic-games
--- claim_gog.sh
+++ claim_gog.sh
@@ -5,4 +5,3 @@ cd ~/free-games-claimer
 
 # Run the Node.js scripts sequentially
 node gog
-node epic-games
